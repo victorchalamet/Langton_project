6e2ea5cf7749c46a3d247a61cbdf40296f8d4ec6
Victor CHALAMET <victorchalamet@gmail.com>
2023-02-12 13:49:55 +0100

    modify setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup (
-    name = "Langton_ant",
+    name = "Langton-project",
     version = "0.1",
     description = "Study of cellular automata",
     long_description = read("README.md"),
     author = "CHALAMET Victor",
     author_email = "victorchalamet@gmail.com",
-    packages = ["ant", "ant_interface"],
+    packages = ["Langton-project", "Langton-project_interface"],
     license = "MIT",
     url = "https://github.com/victorchalamet/Langton-project",
     download_url = "https://github.com/victorchalamet/Langton-project/archive/refs/tags/v_0.1.tar.gz",
