e2828982be1984aeb1548c1f517e79fe34d89598
Victor CHALAMET <victorchalamet@gmail.com>
2023-02-13 11:23:20 +0100

    modify gitignore

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup (
-    name = "Langton-project",
-    version = "0.3",
+    name = "Langton_project",
+    version = "0.1",
     description = "Study of cellular automata",
     long_description = read("README.md"),
     author = "CHALAMET Victor",
@@ -14,7 +14,7 @@ setup (
     packages = ["Langton_project", "Langton_project_interface"],
     license = "MIT",
     url = "https://github.com/victorchalamet/Langton_project",
-    download_url = "https://github.com/victorchalamet/Langton_project/archive/refs/tags/v_0.3.tar.gz",
+    download_url = "https://github.com/victorchalamet/Langton_project/archive/refs/tags/v_0.4.tar.gz",
     keywords = ["emergence", "animation", "data"],
     install_requires = [
         "numpy",
